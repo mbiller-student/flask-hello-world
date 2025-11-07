3bb01864c6f9ebbf76482b01322940aedf71aa48
Matthew Biller <mabi3900@colorado.edu>
2025-11-07 04:02:50 +0000

    db_drop commit

--- app.py
+++ app.py
@@ -9,70 +9,81 @@ def hello_world():
 
 @app.route('/db_test')
 def testing():
-	con = psycopg2.connect("postgresql://matt_postgressql_database_user:NvvhlXi6zQslJ1WpTR1FK7cSxjFouHsU@dpg-d46m656mcj7s73e8su5g-a/matt_postgressql_database")
-	con.close()
-	return "Database Connection Successful"
+    con = psycopg2.connect("postgresql://matt_postgressql_database_user:NvvhlXi6zQslJ1WpTR1FK7cSxjFouHsU@dpg-d46m656mcj7s73e8su5g-a/matt_postgressql_database")
+    con.close()
+    return "Database Connection Successful"
 
 
 @app.route('/db_create')
 def creating():
-	con = psycopg2.connect("postgresql://matt_postgressql_database_user:NvvhlXi6zQslJ1WpTR1FK7cSxjFouHsU@dpg-d46m656mcj7s73e8su5g-a/matt_postgressql_database")
+    con = psycopg2.connect("postgresql://matt_postgressql_database_user:NvvhlXi6zQslJ1WpTR1FK7cSxjFouHsU@dpg-d46m656mcj7s73e8su5g-a/matt_postgressql_database")
 
-	cur = con.cursor()
-	cur.execute('''
-		CREATE TABLE IF NOT EXISTS Basketball (
-		First varchar(255),
-		Last varchar(255),
-		City varchar(255),
-		Name varchar(255),
-		Number int
-		);
-	''')
+    cur = con.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball (
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+        ''')
 
-	con.commit()
-	con.close()
-	return "Basketball Tabel successfully created!"
+    con.commit()
+    con.close()
+    return "Basketball Tabel successfully created!"
 
 
 @app.route('/db_insert')
 def inserting():
-	con = psycopg2.connect("postgresql://matt_postgressql_database_user:NvvhlXi6zQslJ1WpTR1FK7cSxjFouHsU@dpg-d46m656mcj7s73e8su5g-a/matt_postgressql_database")
+    con = psycopg2.connect("postgresql://matt_postgressql_database_user:NvvhlXi6zQslJ1WpTR1FK7cSxjFouHsU@dpg-d46m656mcj7s73e8su5g-a/matt_postgressql_database")
 
-	cur = con.cursor()
-	cur.execute('''
-		INSERT INTO Basketball (First, Last, City, Name, Number)
-		Values
-		('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
-		('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
-		('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
-		('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
-	''')
+    cur = con.cursor()
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+        ''')
 
-	con.commit()
-	con.close()
-	return "Basketball Table successfully populated!"
+    con.commit()
+    con.close()
+    return "Basketball Table successfully populated!"
 
 
 @app.route('/db_select')
 def selecting():
-	con = psycopg2.connect("postgresql://matt_postgressql_database_user:NvvhlXi6zQslJ1WpTR1FK7cSxjFouHsU@dpg-d46m656mcj7s73e8su5g-a/matt_postgressql_database")
+    con = psycopg2.connect("postgresql://matt_postgressql_database_user:NvvhlXi6zQslJ1WpTR1FK7cSxjFouHsU@dpg-d46m656mcj7s73e8su5g-a/matt_postgressql_database")
 
-	cur = con.cursor()
-	cur.execute('''
-		SELECT * FROM Basketball;
-	''')
-	records = cur.fetchall()
-	con.close()
-	response_string = ""
-	response_string += "<table>"
+    cur = con.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+        ''')
+    records = cur.fetchall()
+    con.close()
+    response_string = ""
+    response_string += "<table>"
 
-	for player in records:
-		response_string += "<tr>"
-		for info in player:
-			response_string += "<td>{}</td>".format(info)
-		response_string += "</tr>"
-	response_string += "</table>"
-	return response_string
+    for player in records:
+        response_string += "<tr>"
+        for info in player:
+            response_string += "<td>{}</td>".format(info)
+        response_string += "</tr>"
+    response_string += "</table>"
+    return response_string
 		
 
+@app.route('/db_drop')
+def dropping():
+    con = psycopg2.connect("postgresql://matt_postgressql_database_user:NvvhlXi6zQslJ1WpTR1FK7cSxjFouHsU@dpg-d46m656mcj7s73e8su5g-a/matt_postgressql_database")
 
+    cur = con.cursor()
+    cur.execute('''
+        DROP TABLE Basketball;
+        ''')
+
+    con.commit()
+    con.close()
+    return "Basketball Table successfully dropped!"
