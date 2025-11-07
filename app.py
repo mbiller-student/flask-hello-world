2eb843f5c7056a1d18722692de97ccfed8ba86bb
Matthew Biller <mabi3900@colorado.edu>
2025-11-07 03:13:34 +0000

    db_test commit

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+import psycopg2
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello World from Matt Biller -- in 3308!'
+
+@app.route('/db_test')
+def testing():
+	con = psycopg2postgresql://matt_postgressql_database_user:NvvhlXi6zQslJ1WpTR1FK7cSxjFouHsU@dpg-d46m656mcj7s73e8su5g-a/matt_postgressql_database
